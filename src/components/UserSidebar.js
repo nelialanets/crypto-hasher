796be87673b07e64b639fc43ffe133dbf6b42fb0
X <xac923@outlook.com>
2022-04-24 19:51:01 -0700

    we together styled usersidebar and also wrote the code

--- src/components/UserSidebar.js
+++ src/components/UserSidebar.js
@@ -1,142 +1,109 @@
-import React from "react";
-import Drawer  from "@mui/material/Drawer";
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CryptoState from "../CryptoContext";
-import { signOut } from "firebase/auth";
-import { auth, db } from "../firebase";
-import { AiFillDelete } from "react-icons/ai";
-import { doc, setDoc } from "firebase/firestore";
-
-import { requirePropFactory } from "@mui/material";
+import * as React from 'react';
+import Drawer from '@mui/material/Drawer';
+import { CryptoState } from '../CryptoContext';
+import { Avatar } from '@mui/material';
+import { Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '../index.css'
 
 
-export function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+const logOut = () => {
+
+}
 
 export default function UserSidebar() {
   const [state, setState] = React.useState({
     right: false,
   });
-  const { user, setAlert, watchlist, coins, symbol } = CryptoState();
 
-  console.log(watchlist, coins);
+    const {user} = CryptoState();
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
     }
 
     setState({ ...state, [anchor]: open });
   };
 
-  const logOut = () => {
-    signOut(auth);
-    setAlert({
-      open: true,
-      type: "success",
-      message: "Logout Successfull !",
-    });
-
-    toggleDrawer();
-  };
-
-  const removeFromWatchlist = async (coin) => {
-    const coinRef = doc(db, "watchlist", user.uid);
-    try {
-      await setDoc(
-        coinRef,
-        { coins: watchlist.filter((wish) => wish !== coin?.id) },
-        { merge: true }
-      );
-
-      setAlert({
-        open: true,
-        message: `${coin.name} Removed from the Watchlist !`,
-        type: "success",
-      });
-    } catch (error) {
-      setAlert({
-        open: true,
-        message: error.message,
-        type: "error",
-      });
-    }
-  };
-
   return (
     <div>
-      {["right"].map((anchor) => (
+      {['right'].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Avatar
-            onClick={toggleDrawer(anchor, true)}
-            style={{
-              height: 38,
-              width: 38,
-              marginLeft: 15,
-              cursor: "pointer",
-              backgroundColor: "#EEBC1D",
-            }}
-            src={user.photoURL}
-            alt={user.displayName || user.email}
-          />
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
-            <div>
-              <div>
-                <Avatar
-                  src={user.photoURL}
-                  alt={user.displayName || user.email}
-                />
-                <span
-                  style={{
+            <Avatar 
+                onClick={toggleDrawer(anchor, true)}
+                sx={{
+                    height: 38,
+                    width: 38,
+                    marginLeft: 15,
+                    cursor: "pointer",
+                    backgroundColor: "#29D7B9",
+                    }}
+                    src={user.photoURL}
+                    alt={user.displayName || user.email}
+            />
+            <span
+                style={{
                     width: "100%",
                     fontSize: 25,
                     textAlign: "center",
                     fontWeight: "bolder",
                     wordWrap: "break-word",
-                  }}
-                >
-                  {/* {user.displayName || user.email} */}
-                </span>
-                <div>
-                  <span style={{ fontSize: 15, textShadow: "0 0 5px black" }}>
-                    Watchlist
-                  </span>
-                  {coins.map((coin) => {
-                    if (watchlist.includes(coin.id))
-                      return (
-                        <div>
-                          <span>{coin.name}</span>
-                          <span style={{ display: "flex", gap: 8 }}>
-                            {symbol}{" "}
-                            {numberWithCommas(coin.current_price.toFixed(2))}
-                            <AiFillDelete
-                              style={{ cursor: "pointer" }}
-                              fontSize="16"
-                              onClick={() => removeFromWatchlist(coin)}
-                            />
-                          </span>
-                        </div>
-                      );
-                    else return <></>;
-                  })}
-                </div>
+                }}
+            >
+                {user.displayName || user.email}
+            </span>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            styles={{
+                width: 3500,
+                // padding: 25,
+                // height: 100,
+                // display: 'flex',
+                // flexDirection: 'column',
+                // fontFamily: "monospace",
+            }}>
+              <div className='container'>
+                 {/* - - - - -  C R Y P T O ______ H A S H E R - - - - -  */}
               </div>
-              <Button
-                variant="contained"
-                onClick={logOut}
-              >
-                Log Out
-              </Button>
-            </div>
+              <Avatar 
+                onClick={toggleDrawer(anchor, true)}
+                sx={{
+                    height: 100,
+                    width: 100,
+                    margin: 20,
+                    marginBottom: 5, 
+                    cursor: "pointer",
+                    backgroundColor: "#29D7B9",
+                    }}
+                    src={user.photoURL}
+                    alt={user.displayName || user.email}
+            />
+            <span
+                style={{
+                    width: "100%",
+                    fontSize: 25,
+                    textAlign: "center",
+                    fontWeight: "bolder",
+                    wordWrap: "break-word",
+                }}
+            >
+                {user.displayName || user.email}
+            </span>
+            <Button
+                sx={{
+                    height: 50,
+                    padding: 5,
+                    margin: 10,
+                    backgroundColor: '#29D7B9',
+                    color: 'white',
+                    fontSize: 'larger',
+                    fontWeight: 'bold'
+                }} 
+            >Log Out
+            </Button>
           </Drawer>
         </React.Fragment>
       ))}
